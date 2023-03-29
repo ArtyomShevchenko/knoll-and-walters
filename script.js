987b835ad484895fbfa6ebc7327133c310cf2927
ArtyomShevchenko <artyomshevchenkowarmgray@gmail.com>
2023-03-29 17:47:28 +0300

    add response for chat

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
 // navigation
-const nav = document.querySelector("nav")
-const navContainer = document.getElementById("navigation")
-const doc = document.documentElement
+const nav = document.querySelector("nav");
+const navContainer = document.getElementById("navigation");
+const doc = document.documentElement;
 
-nav.addEventListener("click", event => {
-    if (event.target.className == "nav-link") {
-        for (link of nav.children) {
-            link.classList.remove("nav-link-active")
-        }
-        event.target.classList.add("nav-link-active")
-        nav.classList.remove("nav-active")
-
-        burger.querySelector(".burger").classList.remove("burger-active")
-        navContainer.classList.remove("nav-containeer-active")
-        document.body.classList.remove("disable-scroll")
+nav.addEventListener("click", (event) => {
+  if (event.target.className == "nav-link") {
+    for (link of nav.children) {
+      link.classList.remove("nav-link-active");
     }
-})
+    event.target.classList.add("nav-link-active");
+    nav.classList.remove("nav-active");
 
+    burger.querySelector(".burger").classList.remove("burger-active");
+    navContainer.classList.remove("nav-containeer-active");
+    document.body.classList.remove("disable-scroll");
+  }
+});
 
 // burger
-const burger = document.getElementById("burger")
+const burger = document.getElementById("burger");
 burger.addEventListener("click", (e) => {
-    burger.querySelector(".burger").classList.toggle("burger-active");
-    navContainer.classList.toggle("nav-containeer-active");
-    navContainer.querySelector(".bg").classList.toggle("bg-active");
-    document.body.classList.toggle("disable-scroll")
-})
-
+  burger.querySelector(".burger").classList.toggle("burger-active");
+  navContainer.classList.toggle("nav-containeer-active");
+  navContainer.querySelector(".bg").classList.toggle("bg-active");
+  document.body.classList.toggle("disable-scroll");
+});
 
 // header padding after scroll
-const header = document.querySelector("header")
-doc.style.setProperty("--header-height", `${header.offsetHeight}px`)
+const header = document.querySelector("header");
+doc.style.setProperty("--header-height", `${header.offsetHeight}px`);
 
 // scroll to top button
 // document.getElementById("scrollToTopButton")
@@ -38,13 +36,9 @@ doc.style.setProperty("--header-height", `${header.offsetHeight}px`)
 //         window.scrollTo({ top: 0, behavior: "smooth" })
 //     })
 
-
 // window.addEventListener("scroll", e => {
 //     document.documentElement.style.setProperty("--transformBgY", `${window.scrollY}px`)
 // })
-
-
-
 
 // -----------------str.replace()-----------------
 // const str = "Lorem +380 93 487 77 58 ipsum lorem"
@@ -57,27 +51,37 @@ doc.style.setProperty("--header-height", `${header.offsetHeight}px`)
 //     return e * 2
 // })
 
-
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const formData = new FormData(form)
-    const obj = {}
+  const formData = new FormData(form);
+  const obj = {};
 
-    for (let [key, value] of formData) {
-        obj[key] = value
-    }
+  for (let [key, value] of formData) {
+    obj[key] = value;
+  }
 
-    fetch("http://127.0.0.1:4000",
-        {
-            method: "POST",
-            body: JSON.stringify(obj),
-        }
-    )
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-})
+  fetch("http://127.0.0.1:4000", {
+    method: "POST",
+    body: JSON.stringify(obj),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
 
 // chat
+const chatButton = document.querySelector(".chat-button");
+const chatBody = document.querySelector(".chat-body");
+chatButton.addEventListener("click", (e) => {
+  chatBody.classList.toggle("chat-body-open");
+  chatButton.classList.toggle("chat-button-close");
+  document.body.classList.toggle("disable-scroll");
+});
+
+chatBody.querySelector(".close-btn").addEventListener("click", (e) => {
+  chatBody.classList.toggle("chat-body-open");
+  chatButton.classList.toggle("chat-button-close");
+  document.body.classList.toggle("disable-scroll");
+});
